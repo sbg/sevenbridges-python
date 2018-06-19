5f20615efaebcf7af536e0dbafe622562c12a5d9
Dejan Knezevic <dejan.knezevic@sbgenomics.com>
2018-06-19 10:51:52 +0200

    Fix reload in file as well

--- sevenbridges/models/file.py
+++ sevenbridges/models/file.py
@@ -1,5 +1,6 @@
 import io
 import os
+import copy
 import logging
 import tempfile
 
@@ -334,6 +335,7 @@ class File(Resource):
 
         self._data = resource._data
         self._dirty = resource._dirty
+        self._old = copy.deepcopy(self._data.data)
 
         # If file.metadata = value was executed
         # file object will have attribute _method='PUT', which tells us
