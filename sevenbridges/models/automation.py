e534a6cef3b20dae3d181eb6fede0f83816575b8
Filip Tubic <tubefilip@gmail.com>
2019-10-28 17:29:09 +0100

    fix save method on automation run entity to return instance of a run

--- sevenbridges/models/automation.py
+++ sevenbridges/models/automation.py
@@ -732,7 +732,7 @@ class AutomationRun(Resource):
             logger.info('Saving automation run', extra=extra)
             data = self._api.patch(url=self._URL['get'].format(id=self.id),
                                    data=modified_data).json()
-            return Automation(api=self._api, **data)
+            return AutomationRun(api=self._api, **data)a
         else:
             raise ResourceNotModified()
 
